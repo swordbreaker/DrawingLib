18286cacef0561d045d4350d9b4e6c8c6a46eb5d
Tobias Bollinger <tobias.bollinger@gmail.com>
2022-07-11 22:20:42 +0200

    WIP

--- DrawingLib/Drawings/AutoEncoderRecostructionLoss.cs
+++ DrawingLib/Drawings/AutoEncoderRecostructionLoss.cs
@@ -15,19 +15,19 @@ namespace DrawingLib.Drawings
             var bottleNeckSize = new SizeF(rectSize.Width * 0.75f, rectSize.Height);
 
             // Input
-            var inputCirlce = Circle(cicleRadius) with { Text = "Input" };
+            var inputCirlce = Circle(cicleRadius) with { Text = "Input", FillColor = Colors.Red };
 
             // Encoder
-            var encoder = Box(rectSize) with { Text = "Encoder" };
+            var encoder = Box(rectSize) with { Text = "Encoder", FillColor = Colors.Yellow };
 
             // Bottleneck
-            var bottleNeck = Box(bottleNeckSize) with { Text = "Bottleneck" };
+            var bottleNeck = Box(bottleNeckSize) with { Text = "Bottleneck", FillColor = Colors.Aquamarine };
 
             // Decoder
-            var decoder = Box(rectSize) with { Text = "Decoder" };
+            var decoder = Box(rectSize) with { Text = "Decoder", FillColor = Colors.Yellow };
 
             // Output
-            var outputCirlce = Circle(cicleRadius) with { Text = "Output" };
+            var outputCirlce = Circle(cicleRadius) with { Text = "Output", FillColor = Colors.Red };
 
             var hbox = HBox() with { Marign = 0, ElementMargin = 25 };
             hbox.Add(
@@ -49,12 +49,16 @@ namespace DrawingLib.Drawings
                 c.StrokeSize = 4;
                 c.FillColor = Colors.Black;
 
-                var start = outputCirlce.Transform.Position;
-                var end = inputCirlce.Transform.Position;
-                var p0 = start + (end - start) * 1f + new Vector2(0, 100);
-                var p1 = start + (end - start) * 2 / 3f + new Vector2(0, 100);
-                var p2 = start + (end - start) * 1 / 3f + new Vector2(0, 100);
+                var start = (Vector2)outputCirlce.AnchorPoints.Last() + outputCirlce.Transform.Position;
+                var end = (Vector2)inputCirlce.AnchorPoints.Last() + inputCirlce.Transform.Position;
+                var directionVec = (end - start);
+                var offsetY = new Vector2(0, 100);
+                var p0 = start + directionVec * 1f + offsetY;
+                var p1 = start + directionVec * 2 / 3f + offsetY;
+                var p2 = start + directionVec * 1 / 3f + offsetY;
                 var p4 = start + new Vector2(0, 100);
+
+                var textPos = start + directionVec * 1 / 2 + offsetY + new Vector2(0, 15);
 
                 var reconstructionLossPath = new PathF();
                 reconstructionLossPath.MoveTo(end.X, end.Y);
@@ -62,6 +66,7 @@ namespace DrawingLib.Drawings
                 reconstructionLossPath.LineTo(p2);
                 reconstructionLossPath.QuadTo(p4, start);
                 c.DrawPath(reconstructionLossPath);
+                c.DrawString("Reconstruction Loss", textPos.X, textPos.Y, HorizontalAlignment.Center);
             });
         }
     }
